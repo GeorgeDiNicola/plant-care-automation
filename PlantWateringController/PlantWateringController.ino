f9dff0cd9a7bab3a8d85863511a35d2c90444ceb
George DiNicola <george.j.dinicola@gmail.com>
2024-11-17 15:48:43 -0800

    added functionality for 5V pump and improved error handling

--- PlantWateringController/PlantWateringController.ino
+++ PlantWateringController/PlantWateringController.ino
@@ -16,31 +16,65 @@
 
 #include "Adafruit_seesaw.h"
 #include "RTClib.h"
-
 Adafruit_seesaw ss;
-RTC_DS1307 rtc;  // Declare rtc as an instance of RTC_DS1307
+RTC_DS1307 rtc;
+
+const unsigned long CAP_THRESHOLD = 1000;
+const int RELAY_PIN = 13;
+
+const unsigned long ONE_MINUTE = 60000;
+const unsigned long ONE_HOUR = 60000 * 60;
 
 const unsigned long BAUD_RATE = 115200;
-const unsigned long WAIT_TIME_MS = 60000;  // 1 minute
+const unsigned long WAIT_TIME_MS = ONE_MINUTE;
+const unsigned long PUMP_MIN_TIME_MS = 1000;
+const unsigned long PUMP_MAX_TIME_MS = 30000;
+
+
+void logError(const char* message) {
+  Serial.print(F("Error: "));
+  Serial.print(message);
+}
+
+bool runPumpFor(int seconds) {
+  unsigned long milliseconds = (unsigned long)(seconds * 1000.0);
+
+  if (milliseconds < PUMP_MIN_TIME_MS || milliseconds > PUMP_MAX_TIME_MS) {
+    logError("Pump time needs to be set between 1 and 30 seconds");
+    return false;
+  }
+
+  digitalWrite(RELAY_PIN, HIGH);
+  delay(milliseconds);  
+  digitalWrite(RELAY_PIN, LOW);
+
+  return true;
+}
+
+void stopLoop() {
+  while (true) {
+    delay(WAIT_TIME_MS * 60);
+  }
+}
 
 void setup() {
   Serial.begin(BAUD_RATE);
 
   // Initialize Adafruit STEMMA Soil Sensor
   if (!ss.begin(0x36)) {
-    Serial.println("Error: Adafruit soil sensor not found. Delaying program to avoid excessive CPU usage");
-    while (true) {
-      delay(WAIT_TIME_MS);
-    }
+    logError("Adafruit soil sensor not found. Delaying program to avoid excessive CPU usage");
+    stopLoop();
   }
 
   // Initialize RTC module
   if (!rtc.begin()) {
-    Serial.println("Error: RTC module not found. Delaying program to avoid excessive CPU usage");
-    while (true) {
-      delay(WAIT_TIME_MS);
-    }
+    logError("RTC module not found. Delaying program to avoid excessive CPU usage");
+    stopLoop();
   }
+
+  // Initialize the 5V pump
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, LOW);
 
   rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));  // Set RTC to the compile date and time
 }
@@ -49,7 +83,6 @@ void loop() {
   float tempC = ss.getTemp();
   uint16_t capread = ss.touchRead(0);
 
-  // Output the Current Datetime
   DateTime now = rtc.now();
   char currentDateTime[50];
   sprintf(currentDateTime, "Date and Time (PST): %d/%02d/%02d %02d:%02d:%02d",
@@ -60,9 +93,16 @@ void loop() {
     now.minute(),
     now.second()
   );
+  
   Serial.println(currentDateTime);
-
   Serial.print("Temperature: "); Serial.print(tempC); Serial.println("*C");
   Serial.print("Capacitive: "); Serial.println(capread);
+ 
+  if (capread < CAP_THRESHOLD) {
+    if (!runPumpFor(30)) {
+      stopLoop();
+    }
+  }
+
   delay(WAIT_TIME_MS);
 }
